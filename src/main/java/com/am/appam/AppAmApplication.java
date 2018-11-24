9c9fd69685f60e32ff00cd65d17d73856228e63b
mkbcode <monoj.bisoyi@gmail.com>
2018-11-24 16:39:42 +0530

    Initial Commit

---
+++ src/main/java/com/am/appam/AppAmApplication.java
@@ -0,0 +1,12 @@
+package com.am.appam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAmApplication.class, args);
+	}
+}
