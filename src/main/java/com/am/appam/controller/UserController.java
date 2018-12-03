4b670c442e98b6681e951b575d947b28225f3d14
mkbcode <monoj.bisoyi@gmail.com>
2018-12-03 23:12:04 +0530

    Implemented "JWT" Bearer Token

--- src/main/java/com/am/appam/controller/UserController.java
+++ src/main/java/com/am/appam/controller/UserController.java
@@ -10,4 +10,21 @@ public class UserController {
 	public String getHome() {
 		return "working";
 	}
+	
+	@GetMapping("/api/user/test")
+	public String getTestUser() {
+		return "getTestUser";
+	}
+	
+	@GetMapping("/api/user/example")
+	public String getTestExample() {
+		return "getTestExample";
+	}
+	
+	@GetMapping("/api/user/dummy")
+	public String getTestDummy() {
+		return "getTestDummy";
+	}
+	
+	
 }
